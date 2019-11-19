50aca0fa1b4721689f005ec331d67695d3dab80c
Danni Chen <chendanni@chendannideMacBook-Pro.local>
2019-11-19 14:53:32 -0800

    category

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,16 +6,22 @@ const Home = () => {
     return (
         
         <section>
-            <div className={styles.big_grid_1}><img src="/images/treefrog.jpg"></img></div>
-            <div><a href="/detail/1"><img src="/images/treefrog.jpg" href></img></a></div>
-            <div><a href="/detail/1"><img src="/images/treefrog.jpg" href></img></a></div>
-            <div><a href="/detail/1"><img src="/images/treefrog.jpg" href></img></a></div>
-            <div><a href="/detail/1"><img src="/images/treefrog.jpg" href></img></a></div>
-            <div><a href="/detail/1"><img src="/images/treefrog.jpg" href></img></a></div>
-            <div><a href="/detail/1"><img src="/images/treefrog.jpg" href></img></a></div>
-            <div><a href="/detail/1"><img src="/images/treefrog.jpg" href></img></a></div>
-            <div><a href="/detail/1"><img src="/images/treefrog.jpg" href></img></a></div>
-            <div className={styles.big_grid_2}><img src="/images/treefrog.jpg"></img></div> 
+            <div className={styles.big_grid_1}>
+                <h1>Soup</h1>
+                <a href = "/soup"><img src="/images/treefrog.jpg" href></img></a>
+            </div>
+            <div><a href="/detail/1001"><img src="/images/treefrog.jpg" href></img></a></div>
+            <div><a href="/detail/1002"><img src="/images/treefrog.jpg" href></img></a></div>
+            <div><a href="/detail/1003"><img src="/images/treefrog.jpg" href></img></a></div>
+            <div><a href="/detail/1004"><img src="/images/treefrog.jpg" href></img></a></div>
+            <div><a href="/detail/1005"><img src="/images/treefrog.jpg" href></img></a></div>
+            <div><a href="/detail/1006"><img src="/images/treefrog.jpg" href></img></a></div>
+            <div><a href="/detail/3001"><img src="/images/treefrog.jpg" href></img></a></div>
+            <div><a href="/detail/3002"><img src="/images/treefrog.jpg" href></img></a></div>
+            <div className={styles.big_grid_2}>
+                <h1>Thanksgiving</h1>
+                <a href = "/thanksgiving recipe"><img src="/images/treefrog.jpg" href></img></a>
+            </div> 
         </section>
     )
 }
